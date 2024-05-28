55da1105fa5d6f1c329972de6efa968873b3fac4
Maya <maya.karabulastysiak@gmail.com>
2024-05-28 14:04:09 +0200

    remove logo and time

--- js/index.js
+++ js/index.js
@@ -1,33 +1,3 @@
-// MORI
-
-var incept = 678758400000;
-var termination = 3253914000000;
-
-renderRemainingTime();
-
-function renderRemainingTime() {
-  var curr = Date.now();
-  document.getElementById("mori").value = (curr-incept)/(termination-incept);
-
-  // Just need a rough estimation
-  var delta = Math.floor(((termination-incept)-(curr-incept))/1000);
-  var days = Math.floor(delta / (60*60*24));
-  delta -= days * (60*60*24);
-  var hours = Math.floor(delta / (60*60)) % 24;
-  delta -= hours * (60*60);
-  var minutes = Math.floor(delta / 60) % 60;
-  delta -= minutes * 60;
-  var seconds = delta % 60;
-
-  var total = [days, hours, minutes, seconds]
-  document.getElementById("mori-remaining").innerHTML = "[ " + total.join(" :: ") + " ]";
-}
-
-setInterval(function(){
-  renderRemainingTime();
-}, 1000);
-
-
 // BEORGAS RENDERING
 // Simple1DNoise taken from: https://www.michaelbromley.co.uk/blog/simple-1d-noise-in-javascript/
 
